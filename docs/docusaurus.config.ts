130d33b19f6c7f5df927e1d94bd9df1c92efa4dd
Derrick Wiest <me@dlwiest.com>
2025-11-13 16:36:58 -0500

    Update domain to hass-react.com

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://hass-react.dlwiest.com',
+  url: 'https://hass-react.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For custom domain, this is usually '/'
   baseUrl: '/',
